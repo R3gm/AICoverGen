4075e69427fbafc30b0bc3f1595cc42ec2d4ace0
Davide Rizzotti <helloWorld199@users.noreply.huggingface.co>
2024-03-26 12:05:53 +0000

    device changed to cpu

--- src/main.py
+++ src/main.py
@@ -193,9 +193,9 @@ def preprocess_song(song_input, mdx_model_params, song_id, is_webui, input_type,
 def voice_change(voice_model, vocals_path, output_path, pitch_change, f0_method, index_rate, filter_radius, rms_mix_rate, protect, crepe_hop_length, is_webui):
     rvc_model_path, rvc_index_path = get_rvc_model(voice_model, is_webui)
     device = 'cpu'
-    config = Config(device, False)
+    config = Config(device, True)
     hubert_model = load_hubert(device, config.is_half, os.path.join(rvc_models_dir, 'hubert_base.pt'))
-    cpt, version, net_g, tgt_sr, vc = get_vc(device, False, config, rvc_model_path)
+    cpt, version, net_g, tgt_sr, vc = get_vc(device, config.is_half, config, rvc_model_path)
 
     # convert main vocals
     rvc_infer(rvc_index_path, index_rate, vocals_path, output_path, pitch_change, f0_method, cpt, version, net_g, filter_radius, tgt_sr, rms_mix_rate, protect, crepe_hop_length, vc, hubert_model)
@@ -310,8 +310,10 @@ def song_cover_pipeline(song_input, voice_model, pitch_change, keep_files,
                 if file and os.path.exists(file):
                     os.remove(file)
 
-        return ai_cover_path
+        # Returning the stems: AI cover, original vocal, original instrumental, AI generated vocal
 
+        return ai_cover_path, vocals_path, instrumentals_path, ai_vocals_path
+        
     except Exception as e:
         raise_exception(str(e), is_webui)
 
@@ -343,7 +345,7 @@ if __name__ == '__main__':
     if not os.path.exists(os.path.join(rvc_models_dir, rvc_dirname)):
         raise Exception(f'The folder {os.path.join(rvc_models_dir, rvc_dirname)} does not exist.')
 
-    cover_path = song_cover_pipeline(args.song_input, rvc_dirname, args.pitch_change, args.keep_files,
+    cover_path, original_vocals, original_instrumentals, ai_vocals= song_cover_pipeline(args.song_input, rvc_dirname, args.pitch_change, args.keep_files,
                                      main_gain=args.main_vol, backup_gain=args.backup_vol, inst_gain=args.inst_vol,
                                      index_rate=args.index_rate, filter_radius=args.filter_radius,
                                      rms_mix_rate=args.rms_mix_rate, f0_method=args.pitch_detection_algo,
@@ -353,3 +355,8 @@ if __name__ == '__main__':
                                      reverb_dry=args.reverb_dryness, reverb_damping=args.reverb_damping,
                                      output_format=args.output_format)
     print(f'[+] Cover generated at {cover_path}')
+    print(f'[+] Original vocals at {original_vocals}')
+    print(f'[+] Original instrumentals at {original_instrumentals}')
+    print(f'[+] AI vocals at {ai_vocals}')
+
+
